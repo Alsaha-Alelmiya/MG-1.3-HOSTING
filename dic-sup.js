4f73be44d35c7ff7093db59b9a21ea04efa78bc1
Alsaha-Alelmiya <119962189+Alsaha-Alelmiya@users.noreply.github.com>
2023-03-06 13:48:35 +0300

    Add files via upload

---
+++ dic-sup.js
@@ -0,0 +1,225 @@
+var isDivVisible = false;
+var div1 = document.getElementById("dic-m1");
+var button1 = document.getElementById("dic-btn1");
+
+button1.addEventListener("click", function(){
+if(isDivVisible){
+div1.style.display = "none";
+isDivVisible = true;
+}
+else{
+$("#dic-m1").toggle('fast');
+div1.style.display = "block";
+isDivVisible = false;
+}
+});
+
+var div2 = document.getElementById("dic-m2");
+var button2 = document.getElementById("dic-btn2");
+
+button2.addEventListener("click", function(){
+if(isDivVisible){
+div2.style.display = "none";
+isDivVisible = true;
+}
+else{
+$("#dic-m2").toggle('fast');
+div2.style.display = "block";
+isDivVisible = false;
+}
+});
+
+var div3 = document.getElementById("dic-m3");
+var button3 = document.getElementById("dic-btn3");
+
+button3.addEventListener("click", function(){
+if(isDivVisible){
+div3.style.display = "none";
+isDivVisible = true;
+}
+else{
+$("#dic-m3").toggle('fast');
+div3.style.display = "block";
+isDivVisible = false;
+}
+});
+
+var div4 = document.getElementById("dic-m4");
+var button4 = document.getElementById("dic-btn4");
+
+button4.addEventListener("click", function(){
+if(isDivVisible){
+div4.style.display = "none";
+isDivVisible = true;
+}
+else{
+$("#dic-m4").toggle('fast');
+div4.style.display = "block";
+isDivVisible = false;
+}
+});
+
+var div5 = document.getElementById("dic-m5");
+var button5 = document.getElementById("dic-btn5");
+
+button5.addEventListener("click", function(){
+if(isDivVisible){
+div5.style.display = "none";
+isDivVisible = true;
+}
+else{
+$("#dic-m5").toggle('fast');
+div5.style.display = "block";
+isDivVisible = false;
+}
+});
+
+var div6 = document.getElementById("dic-m6");
+var button6 = document.getElementById("dic-btn6");
+
+button6.addEventListener("click", function(){
+if(isDivVisible){
+div6.style.display = "none";
+isDivVisible = true;
+}
+else{
+$("#dic-m6").toggle('fast');
+div6.style.display = "block";
+isDivVisible = false;
+}
+});
+
+var div7 = document.getElementById("dic-m7");
+var button7 = document.getElementById("dic-btn7");
+
+button7.addEventListener("click", function(){
+if(isDivVisible){
+div7.style.display = "none";
+isDivVisible = true;
+}
+else{
+$("#dic-m7").toggle('fast');
+div7.style.display = "block";
+isDivVisible = false;
+}
+});
+
+var div8 = document.getElementById("dic-m8");
+var button8 = document.getElementById("dic-btn8");
+
+button8.addEventListener("click", function(){
+if(isDivVisible){
+div8.style.display = "none";
+isDivVisible = true;
+}
+else{
+$("#dic-m8").toggle('fast');
+div8.style.display = "block";
+isDivVisible = false;
+}
+});
+
+var div9 = document.getElementById("dic-m9");
+var button9 = document.getElementById("dic-btn9");
+
+button9.addEventListener("click", function(){
+if(isDivVisible){
+div9.style.display = "none";
+isDivVisible = true;
+}
+else{
+$("#dic-m9").toggle('fast');
+div9.style.display = "block";
+isDivVisible = false;
+}
+});
+
+var div10 = document.getElementById("dic-m10");
+var button10 = document.getElementById("dic-btn10");
+
+button10.addEventListener("click", function(){
+if(isDivVisible){
+div10.style.display = "none";
+isDivVisible = true;
+}
+else{
+$("#dic-m10").toggle('fast');
+div10.style.display = "block";
+isDivVisible = false;
+}
+});
+
+var div11 = document.getElementById("dic-m11");
+var button11 = document.getElementById("dic-btn11");
+
+button11.addEventListener("click", function(){
+if(isDivVisible){
+div11.style.display = "none";
+isDivVisible = true;
+}
+else{
+$("#dic-m11").toggle('fast');
+div11.style.display = "block";
+isDivVisible = false;
+}
+});
+
+var div12 = document.getElementById("dic-m12");
+var button12 = document.getElementById("dic-btn12");
+
+button12.addEventListener("click", function(){
+if(isDivVisible){
+div12.style.display = "none";
+isDivVisible = true;
+}
+else{
+$("#dic-m12").toggle('fast');
+div12.style.display = "block";
+isDivVisible = false;
+}
+});
+
+var div13 = document.getElementById("dic-m13");
+var button13 = document.getElementById("dic-btn13");
+
+button13.addEventListener("click", function(){
+if(isDivVisible){
+div13.style.display = "none";
+isDivVisible = true;
+}
+else{
+$("#dic-m13").toggle('fast');
+div13.style.display = "block";
+isDivVisible = false;
+}
+});
+
+var div14 = document.getElementById("dic-m14");
+var button14 = document.getElementById("dic-btn14");
+
+button14.addEventListener("click", function(){
+if(isDivVisible){
+div14.style.display = "none";
+isDivVisible = true;
+}
+else{
+$("#dic-m14").toggle('fast');
+div14.style.display = "block";
+isDivVisible = false;
+}
+});
+
+var div15 = document.getElementById("dic-m15");
+var button15 = document.getElementById("dic-btn15");
+
+button15.addEventListener("click", function(){
+if(isDivVisible){
+div15.style.display = "none";
+isDivVisible = true;
+}
+else{
+$("#dic-m15").toggle('fast');
+div15.style.display = "block";
+isDivVisible = false;
+}
+});
